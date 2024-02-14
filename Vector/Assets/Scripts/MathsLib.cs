90211cc1e55d18c50d9f2c43e4aff766d8d6d67b
Haxosura <123653087+Haxosura@users.noreply.github.com>
2024-02-14 11:01:35 +0000

    MathLib

---
+++ Vector/Assets/Scripts/MathsLib.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathsLib
+{
+    public static float VectorToRadians(Vector2 V)
+    {
+        float rv = 0.0f;
+
+        rv = Mathf.Atan(V.y / V.x);
+
+        return rv;
+    }
+
+    public static Vector2 RandiansToVector(float Angle)
+    {
+        Vector2 rv = new Vector2(Mathf.Cos(Angle), Mathf.Sin(Angle));
+
+        return rv;
+    }
+
+    public static MyVector VectorCrossProduct(MyVector A, MyVector B)
+    {
+        MyVector C = new MyVector(0, 0, 0);
+
+        C.x = A.y * B.z - A.z * B.y;
+        C.y = A.z * B.x - A.x * B.z;
+        C.z = A.x * B.y - A.y * B.x;
+
+        return C;
+    }
+}
