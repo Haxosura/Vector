984bf6e5f7f9ccc67222d041a5f9a5ea88caac57
Haxosura <123653087+Haxosura@users.noreply.github.com>
2024-02-20 10:52:43 +0000

    Transformations Part 1

--- Vector/Assets/Scripts/MathsLib.cs
+++ Vector/Assets/Scripts/MathsLib.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class MathsLib
@@ -64,5 +65,65 @@ public class MathsLib
         C.z = A.x * B.y - A.y * B.x;
 
         return C;
+    }
+}
+
+//Workshop 4
+
+public class Matrix4by4
+{
+    public float[,] values;
+    
+    public Matrix4by4(Vector4 column1, Vector4 column2, Vector4 column3, Vector4 column4)
+    {
+        values[0,0] = column1.x;
+        values[1,0] = column1.y;
+        values[2,0] = column1.z;
+        values[3,0] = column1.w;
+
+        values[0,1] = column2.x;
+        values[1,1] = column2.y;
+        values[2,1] = column2.z;
+        values[3,1] = column2.w;
+
+        values[0,2] = column3.x;
+        values[1,2] = column3.y;
+        values[2,2] = column3.z;
+        values[3,2] = column3.w;
+
+        values[0,3] = column4.x;
+        values[1,3] = column4.y;
+        values[2,3] = column4.z;
+        values[3,3] = column4.w;
+    }
+
+    public Matrix4by4(Vector3 column1, Vector3 column2, Vector3 column3, Vector3 column4)
+    {
+        values = new float[4,4];
+
+        values[0,0] = column1.x;
+        values[1,0] = column1.y;
+        values[2,0] = column1.z;
+        values[3, 0] = 0;
+
+        values[0, 1] = column2.x;
+        values[1, 1] = column2.y;
+        values[2, 1] = column2.z;
+        values[3, 1] = 0;
+
+        values[0, 2] = column3.x;
+        values[1, 2] = column3.y;
+        values[2, 2] = column3.z;
+        values[3, 2] = 0;
+
+        values[0, 3] = column1.x;
+        values[1, 3] = column1.y;
+        values[2, 3] = column1.z;
+        values[3, 3] = 1;
+    }
+
+    public static Vector4 operator *(Matrix4by4 lhs, Vector4 b)
+    {
+        return Vector4.zero;
     }
 }
